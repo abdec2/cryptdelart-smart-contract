ce13b5b1fd0cf85cc74cd43319f49388db8eb87d
abdec2 <abdec2@hotmail.com>
2022-06-27 22:09:20 +0300

    Update Cryptdelart.sol

--- contracts/Cryptdelart.sol
+++ contracts/Cryptdelart.sol
@@ -33,13 +33,17 @@ contract Cryptdelart is ERC1155, Ownable {
     uint256[] public costs = [600, 1000, 1500, 3000, 10000, 100000];
     uint256[] public presaleCost = [480, 800, 1200, 2400, 8000, 80000];
 
+    string public revealedUri = "";
+
+    address[] public whiteListedUsers;
+    address[] public vipUsers;
+
+    bool public unrevealed = false;
+    bool public presale = true;
 
     enum categories{ swimSuit,sleepWear,sportsWear,casualStyle,elegantStyle,exoticStyle }
 
     mapping(categories => uint256) private lastMintedToken;
-
-    bool public unrevealed = false;
-    bool public presale = true;
 
     constructor() ERC1155("") {}
 
@@ -47,7 +51,7 @@ contract Cryptdelart is ERC1155, Ownable {
         _setURI(newuri);
     }
 
-    function mint(categories _category, uint256 mintAmount)
+    function mint(categories _category)
         payable
         public
     {
@@ -55,15 +59,73 @@ contract Cryptdelart is ERC1155, Ownable {
 
         uint256 id = getTokenId(_category);
         
-        _mint(msg.sender, id, mintAmount, ""); 
+        _mint(msg.sender, id, 1, ""); 
     }
 
     function getTokenId(categories _category) internal view returns(uint256) {
+        uint256 lastMintId = lastMintedToken[_category];
+        lastMintId = lastMintId + 1;
+        uint256[] memory tokenArray = getTokenArray(_category);
+        uint256 tokenToMint = tokenArray[0];
+        if(lastMintId < tokenArray[tokenArray.length - 1]) {
+            return lastMintId;
+        }
 
+        return tokenToMint;
     }
 
     function getCost(categories _category) internal view returns(uint256) {
+        if(presale) {
+            return (_category == categories.swimSuit) ? presaleCost[0] : (_category == categories.sleepWear) ? presaleCost[1] : (_category == categories.sportsWear) ? presaleCost[2] : (_category == categories.casualStyle) ? presaleCost[3] : (_category == categories.elegantStyle) ? presaleCost[4] : (_category == categories.exoticStyle) ? presaleCost[5] : 0;
+        }
 
+        return (_category == categories.swimSuit) ? costs[0] : (_category == categories.sleepWear) ? costs[1] : (_category == categories.sportsWear) ? costs[2] : (_category == categories.casualStyle) ? costs[3] : (_category == categories.elegantStyle) ? costs[4] : (_category == categories.exoticStyle) ? costs[5] : 0;
+    }
+
+    function getTokenArray(categories _category) internal view returns(uint256[] memory) {
+        return (_category == categories.swimSuit) ? swimSuit : (_category == categories.sleepWear) ? sleepWear : (_category == categories.sportsWear) ? sportsWear : (_category == categories.casualStyle) ? casualStyle : (_category == categories.elegantStyle) ? elegantStyle : exoticStyle;
+    }
+
+    function isWhitelistedUser(address _address) public view returns(bool, uint256) {
+        for(uint256 i=0; i < whiteListedUsers.length; i++) {
+            if(_address == whiteListedUsers[i]) {
+                return (true, i);
+            }
+        }
+        return (false, 0);
+    }
+
+    function isVipUser(address _address) public view returns(bool, uint256) {
+        for(uint256 j = 0; j < vipUsers.length; j++) {
+            if(_address == vipUsers[j]) {
+                return (true, j);
+            }
+        }
+        return (false, 0);
+    }
+
+    function addWhitelistedUser(address _address) public onlyOwner {
+        whiteListedUsers.push(_address);
+    }
+
+    function addVipUser(address _address) public onlyOwner {
+        vipUsers.push(_address);
+    }
+
+    function removeWhitelistUser(address _address) public onlyOwner {
+        (bool _isWhitelistedUser, uint256 s) = isWhitelistedUser(_address);
+        if(_isWhitelistedUser){
+            whiteListedUsers[s] = whiteListedUsers[whiteListedUsers.length - 1];
+            whiteListedUsers.pop();
+        } 
+    }
+
+    function removeVipUser(address _address) public onlyOwner {
+        (bool _isVipUser, uint256 s) = isVipUser(_address);
+        if(_isVipUser){
+            vipUsers[s] = vipUsers[vipUsers.length - 1];
+            vipUsers.pop();
+        } 
     }
 
     
